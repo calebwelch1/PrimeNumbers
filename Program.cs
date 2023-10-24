acc5a7c1fce237bd4b40dc17b8f663ca98c377d9
Caleb <calebjohn@posteo.net>
2023-10-23 20:39:13 -0400

    simple prime number checker

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        void CheckPrime(int num)
+        {
+            // blah blah blah
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number you want to check:")
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            int divisors = 0;
+
+            for (int i =1; i <=number; i++){
+                if (number % i == 0) {
+                    divisors++;
+                }
+            }
+
+            if (divisors == 2)
+            {
+                Console.WriteLine("The entered number is prime");
+            }
+            else{
+                Console.WriteLine("The entered number is not prime")
+            }
+        }
+    }
+}
+
+// using System;
+
+// namespace PrimeNumbers
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Enter a prime number");
+//             int number;
+//             int divisors = 0;
+//             number = Convert.ToInt32(Console.ReadLine());
+//             for (let i =0; i<=number; i++)
+//             {
+//                 if (number % i == 0)
+//                 {
+//                     divisors++
+//                 }
+//             }
+//         }
+//     }
+// }
